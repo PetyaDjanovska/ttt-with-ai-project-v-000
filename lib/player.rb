0c54c63617b7da5b9d80241865646c16387c2aa7
Petya Djanovska <petya.djanovska@gmail.com>
2018-02-12 15:14:45 +0000

    automatically backed up by learn

--- lib/player.rb
+++ lib/player.rb
@@ -1,13 +1,14 @@
 module Players
-  attr_reader :token
 
-  def initialize(token)
-    @token = token
-  end
 end
 
 class Player
 include Players
+attr_reader :token
+
+def initialize(token)
+  @token = token
+end
 
   def move(number)
     number.to_i
