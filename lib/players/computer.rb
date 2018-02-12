5202d934b8677ba642b3c32b00bf67cabc8daecb
Petya Djanovska <petya.djanovska@gmail.com>
2018-02-12 14:45:54 +0000

    automatically backed up by learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -1,14 +1,8 @@
-module Players
-  class Computer < Player
-
-    def move(board)
-      ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
-    end
-
-
-
-
-
-
+class Computer < Player
+include Players
+  def move(board)
+    ["1", "2", "3", "4", "5", "6", "7", "8", "9"].sample
   end
+
+
 end
